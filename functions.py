5e4565b24fb5e44ea395141ba3639b7f612bc0c9
Gilberto Tumangday <gilberto.tumangday@mail.utoronto.ca>
2025-05-13 19:29:58 -0400

    added walls and stuff

--- functions.py
+++ functions.py
@@ -2,8 +2,9 @@ import pygame
 import classes
 import random
 from globals import SCREEN_WIDTH, SCREEN_HEIGHT
+from map import wall_grid
 
-def event_handling(tank1, tank2, screen, shells):
+def event_handling(tank1, tank2, screen, shells, targets):
     keys = pygame.key.get_pressed()
 
     tank1_x_delta = 0
@@ -24,9 +25,9 @@ def event_handling(tank1, tank2, screen, shells):
         tank1.direction = "right"
         tank1_x_delta = 2
     elif keys[pygame.K_e]:
-        tank1.rotate(1)
+        tank1.rotate(2)
     elif keys[pygame.K_q]:
-        tank1.rotate(-1)
+        tank1.rotate(-2)
     
     if keys[pygame.K_i]:
         tank2.direction = "up"
@@ -41,40 +42,48 @@ def event_handling(tank1, tank2, screen, shells):
         tank2.direction = "right"
         tank2_x_delta = 2
     elif keys[pygame.K_o]:
-        tank2.rotate(1)
+        tank2.rotate(2)
     elif keys[pygame.K_u]:
-        tank2.rotate(-1)
+        tank2.rotate(-2)
 
-    moveBounds1 = tank1.x + tank1_x_delta + tank1.frame_width/2 < SCREEN_WIDTH and tank1.x + tank1_x_delta - tank1.frame_width/2 > 0 and tank1.y + tank1_y_delta + tank1.frame_height/2 < SCREEN_HEIGHT and tank1.y + tank1_y_delta - tank1.frame_height/2 > 0
-    if moveBounds1:
-        tank1.move(tank1_x_delta,tank1_y_delta)
-        tank1.update()  
+    moveBounds1 = (tank1.x + tank1_x_delta + tank1.frame_width/2 < SCREEN_WIDTH and tank1.x + tank1_x_delta - tank1.frame_width/2 > 0 
+                    and tank1.y + tank1_y_delta + tank1.frame_height/2 < SCREEN_HEIGHT and tank1.y + tank1_y_delta - tank1.frame_height/2 > 0)
+    
+    if moveBounds1 and not tank_hits_wall(tank1, tank1_x_delta, tank1_y_delta, targets):
+        tank1.move(tank1_x_delta, tank1_y_delta)
+        tank1.update()
 
-    moveBounds2 = tank2.x + tank2_x_delta + tank2.frame_width/2 < SCREEN_WIDTH and tank2.x + tank2_x_delta - tank2.frame_width/2 > 0 and tank2.y + tank2_y_delta + tank2.frame_height/2 < SCREEN_HEIGHT and tank2.y + tank2_y_delta - tank2.frame_height/2 > 0
-    if moveBounds2:
-        tank2.move(tank2_x_delta,tank2_y_delta)
-        tank2.update()  
+    moveBounds2 = (tank2.x + tank2_x_delta + tank2.frame_width/2 < SCREEN_WIDTH and tank2.x + tank2_x_delta - tank2.frame_width/2 > 0
+                    and tank2.y + tank2_y_delta + tank2.frame_height/2 < SCREEN_HEIGHT and tank2.y + tank2_y_delta - tank2.frame_height/2 > 0)
+    
+    if moveBounds2 and not tank_hits_wall(tank2, tank2_x_delta, tank2_y_delta, targets):
+        tank2.move(tank2_x_delta, tank2_y_delta)
+        tank2.update()
 
 def setup(screen, targets):
-    for targetIndex in range (50):
-        x = random.randint(0,SCREEN_WIDTH)
-        y = random.randint(0,SCREEN_HEIGHT)
-        target = classes.Target(x,y,screen)
-        targets.append(target)
+    for targetX in range (750):
+        for targetY in range (750):
+            if(wall_grid[targetX][targetY] == 1):
+                x = targetX
+                y = targetY
+                target = classes.Target(x,y,screen)
+                targets.append(target)
 
 def drawMenu(screen, running):
+
+    #writing
     font = pygame.font.SysFont(None, 40)  # None uses default font, 40 is font size
     font_colour = (144, 238, 144)
-
     single_player_button = font.render("Single Player", True, font_colour)  # White text
     multiplayer_button = font.render("Multi Player", True, font_colour)  # White text
     controls_button = font.render("Controls", True, font_colour)  # White text
 
+    #clickable rects for the words
     singlePlayerRect = single_player_button.get_rect(center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2 + single_player_button.get_height()/2))
     multiPlayerRect = multiplayer_button.get_rect(center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2+single_player_button.get_height() + 10 + multiplayer_button.get_height()/2))
     controlRect = controls_button.get_rect(center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2 + single_player_button.get_height() + 10 + multiplayer_button.get_height() + 10 + controls_button.get_height()/2))
 
-
+    #handle clicks
     mousePos = pygame.mouse.get_pos()
     spHover = singlePlayerRect.collidepoint(mousePos)
     mpHover = multiPlayerRect.collidepoint(mousePos)
@@ -86,14 +95,15 @@ def drawMenu(screen, running):
     if(coHover):
         controls_button = font.render("Controls", True, (255,255,255))
 
+    #draw everything onto screen
     logo = pygame.image.load("resources/logo.png")
     logo = pygame.transform.scale(logo, (logo.get_width()/2,logo.get_height()/2))
-
     screen.blit(logo,(SCREEN_WIDTH/2 - logo.get_width()/2, SCREEN_HEIGHT/10 - 100))
     screen.blit(single_player_button, (SCREEN_WIDTH/2 - single_player_button.get_width()/2,SCREEN_HEIGHT/2))  # Draw text at position (100, 100)
     screen.blit(multiplayer_button, (SCREEN_WIDTH/2 - multiplayer_button.get_width()/2,SCREEN_HEIGHT/2+single_player_button.get_height() + 10))  # Draw text at position (100, 100)
     screen.blit(controls_button, (SCREEN_WIDTH/2 - controls_button.get_width()/2,SCREEN_HEIGHT/2 + single_player_button.get_height() + 10 + multiplayer_button.get_height() + 10))  # Draw text at position (100, 100)
 
+    #event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return "off"
@@ -105,39 +115,110 @@ def drawMenu(screen, running):
             elif coHover:
                 return "controls"
     
+    #default case
     return "nothing"
 
-def drawGame(screen, background, tank1, tank2, shells, targets):
-    pygame.draw.rect(screen,(211,211,211),background) #background
+def drawGame(screen, background, tank1, tank2, shells, targets, gamestate):
+    font = pygame.font.SysFont(None, 40)  # None uses default font, 40 is font size
+    font_colour = (255, 0, 0)
+    goalText = font.render("Eliminate the opposing tank.", True, font_colour)
 
-    tank1.draw(screen) #tanks
+    #draw the background
+    pygame.draw.rect(screen,(211,211,211),background)
+
+    #draw the tanks
+    tank1.draw(screen)
     tank2.draw(screen)
 
-    for shell in shells: #shells
+    #draw shells
+    for shell in shells:
         if(shell.fired):
+            #targets (single player mode)
             for target in targets:
-                #single player mode
                 if shell.shell_rect.colliderect(target.target):
                     shell.fired = False
                     shell.collided = True
                     target.hit = True
-            
+            #shell tank collisions
             if shell.shell_rect.colliderect(tank1.frame):
                 tank1.intact = False
                 shell.fired = False
                 shell.collided = True
+                gamestate = "tank 2 won"
             if shell.shell_rect.colliderect(tank2.frame):
                 tank2.intact = False
                 shell.fired = False
                 shell.collided = False
+                gamestate = "tank 1 won"
             
+            #update and draw shells
             shell.move()
             shell.draw()
         else:
             shells.remove(shell)
     
+    #targets (single player mode)
     for target in targets:
         if(not target.hit):
             target.draw()
         else:
             targets.remove(target)
+    
+    #draw text
+    screen.blit(goalText, (SCREEN_WIDTH/2 - goalText.get_width()/2, 20))
+
+    return gamestate
+
+def drawEndGame(screen, running, winner):
+    #writing
+    font = pygame.font.SysFont(None, 40)  # None uses default font, 40 is font size
+    font_colour = (144, 238, 144)
+
+    restart_game_button = font.render("Restart", True, font_colour)  # White text
+    menu_button = font.render("Menu", True, font_colour)  # White text
+    winFont = pygame.font.SysFont(None, 100)
+    if(winner == 1):
+        winText = winFont.render("Tank 1 wins!", True, (255,0,0))
+    elif(winner == 2):
+        winText = winFont.render("Tank 2 wins!", True, (255,0,0))
+
+    #clickable rects for the words
+    restartRect = restart_game_button.get_rect(center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2 + restart_game_button.get_height()/2 + 50))
+    menuRect = menu_button.get_rect(center = (SCREEN_WIDTH/2,SCREEN_HEIGHT/2+restart_game_button.get_height() + 10 + menu_button.get_height()/2 + 50))
+
+    #handle clicks
+    mousePos = pygame.mouse.get_pos()
+    restartHover = restartRect.collidepoint(mousePos)
+    menuHover = menuRect.collidepoint(mousePos)
+    if(restartHover):
+        restart_game_button = font.render("Restart", True, (255,255,255))
+    if(menuHover):
+        menu_button = font.render("Menu", True, (255,255,255))
+
+    #draw everything onto screen
+    over = pygame.image.load("resources/gameover.png")
+    over = pygame.transform.scale(over, (over.get_width()/2,over.get_height()/2))
+    screen.blit(over,(SCREEN_WIDTH/2 - over.get_width()/2, SCREEN_HEIGHT/10 - 100))
+    screen.blit(restart_game_button, (SCREEN_WIDTH/2 - restart_game_button.get_width()/2,SCREEN_HEIGHT/2 + 50))  # Draw text at position (100, 100)
+    screen.blit(menu_button, (SCREEN_WIDTH/2 - menu_button.get_width()/2,SCREEN_HEIGHT/2+restart_game_button.get_height() + 10 + 50))  # Draw text at position (100, 100)
+    screen.blit(winText, (SCREEN_WIDTH/2 - winText.get_width()/2, SCREEN_HEIGHT - 100 - winText.get_height()))
+
+    #event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return "off"
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if restartHover:
+                return "restart"
+            elif menuHover:
+                return "menu"
+    
+    #default case
+    return "nothing"
+
+def tank_hits_wall(tank, dx, dy, targets):
+    future_frame = tank.frame.move(dx, dy)
+    for target in targets:
+        if future_frame.colliderect(target.target):
+            return True
+    return False
--- main.py
+++ main.py
@@ -19,48 +19,76 @@ tank2.barrel_original.fill((111, 17, 111))
 gamestate = "menu"
 shells = []
 targets = []
-#functions.setup(screen, targets)
+functions.setup(screen, targets)
 
 
 #game loop
 while running:
     screen.fill((53, 94, 59))
 
-    #event handling
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_f:
-                shell = classes.Shell(tank1, screen)
-                shell.fired = True
-                shells.append(shell)
-            if event.key == pygame.K_SEMICOLON:
-                shell = classes.Shell(tank2,screen)
-                shell.fired = True
-                shells.append(shell)
-        
-    functions.event_handling(tank1, tank2, screen, shells)
-
-    #update
-
-    #game rendering here
-    
-    #drawing stuff
+    #state manager
     if gamestate == "menu":
         nextState = functions.drawMenu(screen, running)
         if(nextState == "single player"):
-            gamestate = "play"
+            print("single player")
         elif(nextState == "multiplayer"):
-            gamestate = "play"
+            gamestate = "multiplayer"
         elif(nextState == "controls"):
-            gamestate = "play"
+            print("controls")
         elif nextState == "off":
             running = False
-    elif gamestate == "play":
-        functions.drawGame(screen, background, tank1, tank2, shells, targets)
+    elif gamestate == "multiplayer":        
+        #event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_f:
+                    shell = classes.Shell(tank1, screen)
+                    shell.fired = True
+                    shells.append(shell)
+                if event.key == pygame.K_SEMICOLON:
+                    shell = classes.Shell(tank2,screen)
+                    shell.fired = True
+                    shells.append(shell)
+            
+        functions.event_handling(tank1, tank2, screen, shells, targets)
 
-
+        #draw everything
+        gamestate = functions.drawGame(screen, background, tank1, tank2, shells, targets, gamestate)
+    elif gamestate == "tank 1 won" or gamestate == "tank 2 won":
+        status = ""
+        if(gamestate == "tank 1 won"):
+            status = functions.drawEndGame(screen,running,1)
+        elif(gamestate == "tank 2 won"):
+            status = functions.drawEndGame(screen,running,2)
+        if(status == "off"):
+            running = False
+        elif(status == "restart"):
+            tank1 = classes.Tank(SCREEN_WIDTH/3,SCREEN_HEIGHT/2,screen)
+            tank2 = classes.Tank(SCREEN_WIDTH*2/3,SCREEN_HEIGHT/2,screen)
+            tank2.frame_colour = (202, 161, 196)
+            tank2.roof_colour = (124, 150, 172)
+            tank2.lid_colour = (135, 121, 148)
+            tank2.barrel_original.fill((111, 17, 111))
+            gamestate = "multiplayer"
+            shells = []
+            targets = []
+            functions.setup(screen, targets)
+        elif(status == "menu"):
+            functions.setup(screen, targets)
+            tank1 = classes.Tank(SCREEN_WIDTH/3,SCREEN_HEIGHT/2,screen)
+            tank2 = classes.Tank(SCREEN_WIDTH*2/3,SCREEN_HEIGHT/2,screen)
+            tank2.frame_colour = (202, 161, 196)
+            tank2.roof_colour = (124, 150, 172)
+            tank2.lid_colour = (135, 121, 148)
+            tank2.barrel_original.fill((111, 17, 111))
+            gamestate = "menu"
+            shells = []
+            targets = []
+            functions.setup(screen, targets)
+    
+    #general updating
     pygame.display.flip() #screen buffering
     clock.tick(60) #fps
 
